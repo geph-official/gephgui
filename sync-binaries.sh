f964f6e3c5061aa143f62ce46942d234b5f01581
nullchinchilla <nullchinchilla@pm.me>
2020-03-28 19:49:26 -0400

    3.5.2 final

--- sync-binaries.sh
+++ sync-binaries.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PREFIX="https://binaries.geph.io"
-export VERSION="v0.19.6"
+export VERSION="v0.19.7"
 
 curl "$PREFIX/geph-client-windows-i386-$VERSION.exe" > binaries/win-ia32/geph-client.exe
 curl "$PREFIX/geph-client-linux-i386-$VERSION" > binaries/linux-ia32/geph-client
