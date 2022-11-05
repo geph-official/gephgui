85c879ea805490f2f5bccc3f99a60a87485ad0ff
nullchinchilla <nullchinchilla@pm.me>
2022-11-05 13:49:04 -0500

    much legacy wow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-
+import legacy from "@vitejs/plugin-legacy";
 import dsv from "@rollup/plugin-dsv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [svelte(), dsv()],
+  plugins: [
+    svelte(),
+    dsv(),
+    legacy({
+      targets: ["defaults", ">0.01%"],
+    }),
+  ],
 });
