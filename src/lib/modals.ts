3ba55d0af49994c04d8592c7a6d4dc908c3cc3e5
nullchinchilla <nullchinchilla@pm.me>
2023-05-03 12:18:58 -0500

    Add scaffolding for modals, but not the actual modal display yet

---
+++ src/lib/modals.ts
@@ -0,0 +1,42 @@
+import { writable, type Writable } from "svelte/store";
+
+const loadingContent: Writable<string | null> = writable(null);
+
+const errorContent: Writable<string | null> = writable(null);
+
+/**
+ * Runs the provided async function, showing a loading spinner while the function runs.
+ *
+ * @param {string} label - The text to display while the spinner is active.
+ * @param {() => Promise<any>} f - The async function to be executed.
+ * @returns {Promise<void>}
+ */
+export const runWithSpinner = async (
+  label: string,
+  f: () => Promise<any>
+): Promise<void> => {
+  loadingContent.set(label);
+  try {
+    await f();
+  } finally {
+    loadingContent.set(null);
+  }
+};
+
+/**
+ * Shows an error modal with the given message and resolves the promise when the modal is closed.
+ *
+ * @param {string} s - The error message to be displayed in the modal.
+ * @returns {Promise<void>}
+ */
+export const showErrorModal = (s: string) => {
+  new Promise<void>((resolve, reject) => {
+    errorContent.set(s);
+    let us = errorContent.subscribe((s) => {
+      if (s === null) {
+        resolve();
+        us();
+      }
+    });
+  });
+};
